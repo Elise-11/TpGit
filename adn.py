281133c50d11956251b6f55e35c5d562042d9dce
Elise FABRE <alain.connu@riseup.net>
2021-01-11 21:34:17 +0100

    initial version2

---
+++ adn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    documentation here
+"""
+
+__author__ = 'Elise FABRE'
+
+
+
+def is_valid(adn_str):
+    pass
+
+
+def get_valid_adn(prompt='chaîne : '):
+    pass
+
