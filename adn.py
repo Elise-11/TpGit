42a833d31fc7d32cec17000b55d16c5f881504dd
Elise FABRE <elise.fabre@outlook.com>
2021-01-12 08:41:49 +0100

    functions in adn.py

--- adn.py
+++ adn.py
@@ -9,10 +9,17 @@ __author__ = 'Elise FABRE'
 
 
 
-def is_valid(adn_str):
-    pass
+def is_valid(dna_str):
+    valid ='atcg'
+    for letter in dna_str:
+        if letter not in valid:
+            return False
+    return True 
+    
+ 
+def get_valid_dna() :
+    while not is_valid(input("Entrer une chaîne ADN :")) :
+        print("DNA chain is not valid \n please enter a new chain !")
+    print("DNA chain is valid!")
 
-
-def get_valid_adn(prompt='chaîne : '):
-    pass
 
