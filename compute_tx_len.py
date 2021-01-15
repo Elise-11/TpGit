7f2c225e7393f87b88d175c33f6e96546c04bf78
Elise FABRE <elise.fabre@outlook.com>
2021-01-15 14:57:24 +0100

    pylint 10/10

--- compute_tx_len.py
+++ compute_tx_len.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''This code takes as input a gtf type file and return the length for each transcripts '''
+
+'''This code takes as input a gtf type file and return the length for each transcripts'''
 import re
 
 transcript_start = dict()
 transcript_end = dict()
 
 def get_genomic_length(input_file=None):
-    ''' This function record the smallest value for each transcript in the dictionary named transcript_start,
-    and the highest value in the dictionary named transcript_end. the subtraction of the 2 values ​​
+    ''' This function record the smallest value for each transcript in
+    the transcript_start dictionary, and the highest value in the
+    transcript_end dictionary. the subtraction of the 2 values
     gives the length for each transcript'''
     file_handler = open(input_file)
     for line in file_handler:
-        token = line.split("\t")
-        start = int(token[3])  # le début de l'élément courant
-        end = int(token[4])  # la fin de l'élément courant
-        # L'identifiant du transcrit
+        token = line.split("\t") #split by tabs
+        start = int(token[3]) #start of current element
+        end = int(token[4]) #end of current element
 
-        tx_id = re.search('transcript_id "([^"]+)"', token[8]).group(1)
+        tx_id = re.search('transcript_id "([^"]+)"', token[8]).group(1) #transcript identifier
 
         if tx_id not in transcript_start:
 
@@ -35,7 +36,6 @@ def get_genomic_length(input_file=None):
 
     for tx_id in transcript_start:
         print(tx_id + "\t" + str(transcript_end[tx_id] - transcript_start[tx_id] + 1))
-
 
 if __name__ == '__main__':
     get_genomic_length(input_file='../pymetacline/data/gtf/simple.gtf')
